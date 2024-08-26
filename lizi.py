f9f79a22cd023ed3cfb126ce32b5e5ea0a0a5099
李杰 <8733347+ljcbd1234@user.noreply.gitee.com>
2024-08-26 21:01:46 +0800

    989

--- lizi.py
+++ lizi.py
@@ -1,11 +1,1 @@
-num1 = 5
-if(int(input("请输入第一个数字:"))) == num1:
-    print("恭喜你，首次久猜对了")
-elif(int(input("猜错了，再猜一次:"))) == num1:
-    print("第二次猜对了")
-elif (int(input("猜错了，再猜一次:"))) == num1:
-    print("最后一次猜对了")
-else:
-    print("都猜错了")
-
-    print("hello world")
+print("weeeee")
